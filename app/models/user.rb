b0d0be9969367019969287a9167388be9d9c73ba
Gary Yau <gary.yau@hotmail.com>
2022-02-10 20:34:30 +0900

    validation for first and last name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-
+  validates :first_name, :last_name, presence: true
 end
