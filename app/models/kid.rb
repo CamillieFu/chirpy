1be413c16feb7c77ee85714b571d9776bc26fe0f
Keiran Brown <keiranbrown1301@gmail.com>
2022-02-10 20:45:44 +0900

    removed ethnicity

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -2,6 +2,6 @@ class Kid < ApplicationRecord
   belongs_to :user
   belongs_to :dictionary
 
-  validates :first_name, :last_name, :age, :ethnicity, :birthday, presence: true
+  validates :first_name, :last_name, :age, :birthday, presence: true
   validates :age, numericality: { only_integer: true }
 end
