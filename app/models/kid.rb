f92520d029db7532728d41df0f327ea5a7f43f65
Keiran Brown <keiranbrown1301@gmail.com>
2022-02-10 20:37:18 +0900

    kids validations done (?)

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -1,4 +1,9 @@
 class Kid < ApplicationRecord
   belongs_to :user
   belongs_to :dictionary
+
+  validates :first_name, :last_name, :age, :ethnicity, :birthday, presence: true
+  validates :age, numericality: { only_integer: true }
+  validates :gender_identity, inclusion: { in: %w[Male Female Other] }
+  validates :sexual_orientation, inclusion: { in: %w[Heterosexual Homosexual Pansexual Bisexual Asexual] }
 end
