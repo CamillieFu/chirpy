6b2bd28b5a40d3994961583ed6090d79c4760ee8
Keiran Brown <keiranbrown1301@gmail.com>
2022-02-10 20:43:07 +0900

    removed the validations for gender and sexual orientation

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -4,6 +4,4 @@ class Kid < ApplicationRecord
 
   validates :first_name, :last_name, :age, :ethnicity, :birthday, presence: true
   validates :age, numericality: { only_integer: true }
-  validates :gender_identity, inclusion: { in: %w[Male Female Other] }
-  validates :sexual_orientation, inclusion: { in: %w[Heterosexual Homosexual Pansexual Bisexual Asexual] }
 end
