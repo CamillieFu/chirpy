1870210d6e76caf038f7392f7cc222d9dedad9df
Camille Fujisaki <cae224@cornell.edu>
2022-02-12 16:02:37 +0900

    added stuff for the method

--- app/services/IBM_tone_analyzer/tones.rb
+++ app/services/IBM_tone_analyzer/tones.rb
@@ -4,7 +4,7 @@ require "ibm_watson/authenticators"
 require "ibm_watson/tone_analyzer_v3"
 
 class IBMToneAnalyzer::Tones
-  def self.analyze_tone(text)
+  def self.analyze_tone(json)
 
     authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
       apikey: ENV["TONE_ANALYZER_APIKEY"]
@@ -16,11 +16,19 @@ class IBMToneAnalyzer::Tones
     )
     tone_analyzer.service_url = ENV["TONE_ANALYZER_URL"]
 
-    utterances = [
-      {
-        "text" => text
-      }
-    ]
-    tone_analyzer.tone_chat(utterances: utterances).result["utterances_tone"][0]["tones"]
+    # utterances = [
+    #   {
+    #     "text" => text
+    #   }
+    # ]
+    # tone_analyzer.tone_chat(utterances: utterances).result["utterances_tone"][0]["tones"]
+    tweets = JSON.parse(json)['tweets']
+    tweets.each do |tweet|
+      tone = tone_analyzer.tone(
+      tone_input: tweet["text"],
+      content_type: "text/html"
+    ).result
+    p tone["document_tone"]["tones"]
+    end
   end
 end
