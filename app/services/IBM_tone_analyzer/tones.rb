923557ed2c3800fce7c314e29a577b923eea8f2b
Gary Yau <gary.yau@hotmail.com>
2022-02-12 14:23:33 +0900

    changed the tone analyzer method to general endpoint

--- app/services/IBM_tone_analyzer/tones.rb
+++ app/services/IBM_tone_analyzer/tones.rb
@@ -16,11 +16,6 @@ class IBMToneAnalyzer::Tones
     )
     tone_analyzer.service_url = ENV["TONE_ANALYZER_URL"]
 
-    utterances = [
-      {
-        "text" => text
-      }
-    ]
-    tone_analyzer.tone_chat(utterances: utterances).result["utterances_tone"][0]["tones"]
+    tone_analyzer.tone(tone_input: text, content_type: "text/plain").result["document_tone"]["tones"][0]
   end
 end
