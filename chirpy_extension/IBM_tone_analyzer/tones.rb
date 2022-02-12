6935e09e66631446e0f6a43cf1f4c473ec35b2e8
Camille Fujisaki <cae224@cornell.edu>
2022-02-12 14:16:05 +0900

    just want to add the files

--- chirpy_extension/IBM_tone_analyzer/tones.rb
+++ chirpy_extension/IBM_tone_analyzer/tones.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-
 require "json"
 require "ibm_watson"
 require "ibm_watson/authenticators"
@@ -7,7 +6,7 @@ require "ibm_watson/tone_analyzer_v3"
 
 # If using IAM
 authenticator = IBMWatson::Authenticators::IamAuthenticator.new(
-  apikey: "Your API key"
+  apikey: ENV["TONE_ANALYZER_APIKEY"]
 )
 
 # If you have username & password in your credentials use:
@@ -20,7 +19,7 @@ tone_analyzer = IBMWatson::ToneAnalyzerV3.new(
   authenticator: authenticator,
   version: "2017-09-21"
 )
-tone_analyzer.service_url = "URL"
+tone_analyzer.service_url = ENV["TONE_ANALYZER_URL"]
 
 utterances = [
   {
@@ -35,20 +34,20 @@ utterances = [
 p "\ntone_chat example 1:\n"
 puts JSON.pretty_generate(tone_analyzer.tone_chat(utterances: utterances).result)
 
-p "\ntone example 1:\n"
-puts JSON.pretty_generate(tone_analyzer.tone(
-  tone_input: "I am very happy. It is a good day.",
-  content_type: "text/plain"
-).result)
+# p "\ntone example 1:\n"
+# puts JSON.pretty_generate(tone_analyzer.tone(
+#   tone_input: "I am very happy. It is a good day.",
+#   content_type: "text/plain"
+# ).result)
 
-p "\ntone example 2:\n"
-File.open(Dir.getwd + "/tone-example.json") do |tone_json|
-  tone = tone_analyzer.tone(
-    tone_input: JSON.parse(tone_json.read)["text"],
-    content_type: "text/plain"
-  ).result
-  puts JSON.pretty_generate(tone)
-end
+# p "\ntone example 2:\n"
+# File.open(Dir.getwd + "/tone-example.json") do |tone_json|
+#   tone = tone_analyzer.tone(
+#     tone_input: JSON.parse(tone_json.read)["text"],
+#     content_type: "text/plain"
+#   ).result
+#   puts JSON.pretty_generate(tone)
+# end
 
 # p "\ntone example 3:\n"
 # File.open(Dir.getwd + "/tone-example.json") do |tone_json|
@@ -70,11 +69,13 @@ end
 # end
 
 # p "\ntone example 5:\n"
-# File.open(Dir.getwd + "/tone-example-html.json") do |tone_html|
-#   tone = tone_analyzer.tone(
-#     tone_input: JSON.parse(tone_html.read)["text"],
-#     content_type: "text/html"
-#   ).result
-#   puts tone["document_tone"]["tones"][0]["tone_name"]
-#   # puts JSON.pretty_generate(tone.document_tone)
-# end
+File.open("./fake_texts.json") do |tone_html|
+  tone_html.each do
+    tone = tone_analyzer.tone(
+      tone_input: JSON.parse(tone_html.read)["tweets"]["text"],
+      content_type: "text/html"
+    ).result
+    puts tone["document_tone"]["tones"][0]["tone_name"]
+    puts JSON.pretty_generate(tone.document_tone)
+  end
+end
