b030d19a83ee84d31047a9d3a8b7e9e158dacf40
Camille Fujisaki <cae224@cornell.edu>
2022-02-14 11:08:46 +0900

    cleaned up

--- app/services/test.rb
+++ app/services/test.rb
@@ -12,38 +12,3 @@ twats = weets["utterances_tone"].map do |tweet|
 end
 p tweets = twats.flatten
     # --------------------------------- #
-    Statistic.create(
-      total_tweets: tweets.count,
-      angry_tweets: tweets.count { |tweet| tweet["tone_id"] == "angry" },
-      sad_tweets: tweets.count { |tweet| tweet["tone_id"] == "sad" },
-      joyful_tweets: tweets.count { |tweet| tweet["tone_id"] == "joy" },
-      fearful_tweets: tweets.count { |tweet| tweet["tone_id"] == "fear" },
-      analytical_tweets: tweets.count { |tweet| tweet["tone_id"] == "analytical" },
-      tentative_tweets: tweets.count { |tweet| tweet["tone_id"] == "tentative" },
-      confident_tweets: tweets.count { |tweet| tweet["tone_id"] == "confident" }
-    )
-
-
-    [
-        {
-          "score": 0.79513,
-          "tone_id": "impolite",
-          "tone_name": "Impolite"
-        },
-        {
-          "score": 0.647368,
-          "tone_id": "sad",
-          "tone_name": "Sad"
-        },
-        {
-          "score": 0.67376,
-          "tone_id": "excited",
-          "tone_name": "Excited"
-        },
-        {
-          "score": 0.722718,
-          "tone_id": "satisfied",
-          "tone_name": "Satisfied"
-        }
-      ]
-      ]
