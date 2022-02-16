33cc6ee3de90bf274aa4c54697ce3738af23cdd8
Gary Yau <gary.yau@hotmail.com>
2022-02-17 00:43:46 +0900

    stats controller with index, show, update, create

--- app/controllers/api/v1/statistics_controller.rb
+++ app/controllers/api/v1/statistics_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::StatisticsController < Api::V1::BaseController
-  before_action :set_statistic, only: [ :show ]
+  acts_as_token_authentication_handler_for User, except: [ :index, :show ]
+  before_action :set_statistic, only: [ :show, :update ]
 
   def index
     @statistics = policy_scope(Statistic)
@@ -8,10 +9,38 @@ class Api::V1::StatisticsController < Api::V1::BaseController
   def show
   end
 
+  def update
+    if @statistic.update(statistic_params)
+      render :show
+    else
+      render_error
+    end
+  end
+
+  def create
+    @statistic = Statstatistic.new(statistic_params)
+    @statistic.kid_id = current_user.kid.id
+    authorize @statistic
+    if @statistic.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
   private
 
   def set_statistic
     @statistic = Statistic.find(params[:id])
     authorize @statistic  # For Pundit
+  end
+
+  def statistic_params
+    params.require(:statistic).permit(:total_tweets, :angry_tweets, :sad_tweets, :fearful_tweets, :joyful_tweets, :analytical_tweets, :confident_tweets, :tentative_tweets, :kid_id)
+  end
+
+  def render_error
+    render json: { errors: @statistic.errors.full_messages },
+      status: :unprocessable_entity
   end
 end
