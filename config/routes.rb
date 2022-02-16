80943b42307a4ef6678041901e240b0652d2bfbc
Gary Yau <gary.yau@hotmail.com>
2022-02-17 00:45:32 +0900

    added routes for api with namespace

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :statistics, only: %i[index show create]
+      resources :statistics, only: %i[index show update create]
     end
   end
 end
