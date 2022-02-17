381f4239b2c3b915da46f4456bf7b5addb1967d2
Camille Fujisaki <cae224@cornell.edu>
2022-02-17 20:35:44 +0900

    heroku delete

--- app/services/IBM_tone_analyzer/tones.rb
+++ app/services/IBM_tone_analyzer/tones.rb
@@ -15,29 +15,7 @@ class IBMToneAnalyzer::Tones
     )
     tone_analyzer.service_url = ENV["TONE_ANALYZER_URL"]
 
-    # tweets = JSON.parse(json)['tweets']
-    # tweets.each do |tweet|
-    #   tone = tone_analyzer.tone(
-    #   tone_input: tweet["text"],
-    #   content_type: "text/html"
-    # ).result
-    # p tone["document_tone"]["tones"]
-    # end
+    tone_analyzer.tone(tone_input: text, content_type: "text/plain").result["document_tone"]["tones"][0]
 
-  # tone_analyzer.tone(tone_input: text, content_type: "text/plain").result["document_tone"]["tones"][0]
-
-  # needed File.open just becuz we dont have twitter data yet
-    # File.open("./fake_texts.json") do |tone_html|
-    #   tweets = JSON.parse(tone_html.read)['tweets']
-    #   tweets.each do |tweet|
-    #     tone = tone_analyzer.tone(
-    #       tone_input: tweet["text"],
-    #       content_type: "text/html"
-    #     ).result
-    #     tone["document_tone"]["tones"]
-        # do we need to save this data in a variable? or perhaps do map instead of each?
-        # what data type does this return? if we make it return an array of hashes that might be good
-      end
-    end
   end
 end
