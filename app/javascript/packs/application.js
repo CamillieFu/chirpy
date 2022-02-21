89c91183e63730ffcf7a6b6aaf0e7c7e525196ca
Gary Yau <gary.yau@hotmail.com>
2022-02-21 22:14:25 +0900

    adding js to calculate the age from DOB.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,8 +23,11 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
+import { initCalc_age } from "../plugins/calc_age";
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initCalc_age();
+
 });
