5b5a363e9b6ff6fc87af85627a50079a4e12fc33
Gary Yau <gary.yau@hotmail.com>
2022-02-26 14:12:27 +0900

    extension stylying and debugging

--- chirpy_extension/Chirpy_chrome_extension/scripts/get_tweets.js
+++ chirpy_extension/Chirpy_chrome_extension/scripts/get_tweets.js
@@ -1,21 +1,40 @@
-// const initRemoveTweet = () => {
-//   const tweets = []
-//   const article = document.querySelectorAll("article").forEach((tweet) => {
-//     // if (tweet.innerText.includes("tomorrow")) {
-//     //   tweet.remove()
-//     // }
-//     tweets.push(tweet.innerText)
+// const interval = setInterval(function () {
+//   document.querySelectorAll("article").forEach((tweet) => {
+//     let c_list = Array.from(tweet.classList);
+//     if (!c_list.includes("checked")) {
+//       fetch("http://localhost:3000/api/v1/statistics", {
+//         method: "POST",
+//         body: JSON.stringify({
+//           content: tweet.innerText,
+//         }),
+//         headers: {
+//           "Content-type": "application/json; charset=UTF-8",
+//           "X-User-Email": "mom@gmail.com",
+//           "X-User-Token": "ssDfQhr11iByJFxJCn4W",
+//         },
+//       })
+//         .then((response) => response.json())
+//         .then((data) => {
+//           if (data["bad"] == "true") {
+//             tweet.style.display = "none";
+//             tweet.classList.add("checked");
+//             console.log("bad");
+//           } else {
+//             tweet.classList.add("checked");
+//             console.log("good");
+//           }
+//         })
+//         .catch((error) => {
+//           console.error(
+//             "There has been a problem with your fetch operation:",
+//             error
+//           );
+//         });
+//     }
+//   });
+// }, 10000);
 
-//     // fetch(application/json)
-//     // if true
-//     // tweets.remove
-//   })
-//   const tweets_string = tweets.join()
-// };
-
-// document.querySelectorAll("article").forEach((tweet) => {
-//   const article = tweet.innerText
-//   })
+// interval;
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "get_tweets") get_tweets();
