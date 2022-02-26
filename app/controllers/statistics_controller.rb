369f07797e118c4b2f4eee97d07464250a1349dd
Camille Fujisaki <cae224@cornell.edu>
2022-02-26 11:13:59 +0900

    commented out

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,22 +1,22 @@
 class StatisticsController < ApplicationController
   def create
-    tones = [
-      {
-        "score": 0.685309,
-        "tone_id": "anger",
-        "tone_name": "Anger"
-      },
-      {
-        "score": 0.568183,
-        "tone_id": "fear",
-        "tone_name": "Fear"
-      },
-      {
-        "score": 0.724923,
-        "tone_id": "confident",
-        "tone_name": "Confident"
-      }
-    ]
+    # tones = [
+    #   {
+    #     "score": 0.685309,
+    #     "tone_id": "anger",
+    #     "tone_name": "Anger"
+    #   },
+    #   {
+    #     "score": 0.568183,
+    #     "tone_id": "fear",
+    #     "tone_name": "Fear"
+    #   },
+    #   {
+    #     "score": 0.724923,
+    #     "tone_id": "confident",
+    #     "tone_name": "Confident"
+    #   }
+    # ]
     @statistic = Statistic.new
     authorize @statistic
     tweet_scores = tones.map { |tone| tone[:score] }
@@ -29,31 +29,3 @@ class StatisticsController < ApplicationController
     # currently it is only the highest score regardless of affect
   end
 end
-
-
-tones = [
-  {
-    "score": 0.685309,
-    "tone_id": "anger",
-    "tone_name": "Anger"
-  },
-  {
-    "score": 0.568183,
-    "tone_id": "fear",
-    "tone_name": "Fear"
-  },
-  {
-    "score": 0.724923,
-    "tone_id": "confident",
-    "tone_name": "Confident"
-  }
-]
-@statistic = Statistic.new
-authorize @statistic
-# tweettones = tones.map do |tone|
-#   [tone[:tone_name], tone[:score]]
-# end
-tweet_scores = tones.map { |tone| tone[:score] }
-selected_tone = tones.select { |tone| tone[:score] == tweet_scores.max }
-tone = selected_tone[0][:tone_id]
-@statistic.tone = tone
