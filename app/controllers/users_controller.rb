354ce29441c9f69214416e86687509044d9a9bf7
Gary Yau <gary.yau@hotmail.com>
2022-03-01 22:49:01 +0900

    permiet first and last name at registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :tag_list) ## Rails 4 strong params usage
+    params.require(:user).permit(:tag_list) ## Rails 4 strong params usage
   end
   # this was the documentation but not sure how it will play with devise
 end
