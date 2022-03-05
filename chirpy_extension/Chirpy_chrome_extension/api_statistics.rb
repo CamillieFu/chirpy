23b111534c5d8fb900d593ec480570744d23e8a7
Gary Yau <gary.yau@hotmail.com>
2022-03-05 10:47:22 +0900

    change to constant

--- chirpy_extension/Chirpy_chrome_extension/api_statistics.rb
+++
@@ -1,4 +0,0 @@
-# Ruby integration: uses api data to record stats (DB)
-# on various factors (daily/total removed tweets, emotional
-# valence of removed tweets, tweets which contained words from
-# their dictionaries)
