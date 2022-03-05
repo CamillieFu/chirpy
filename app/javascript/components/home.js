6ebb6489d965dc440213db9f2d43e32a3448244c
Keiran Brown <keiranbrown1301@gmail.com>
2022-03-05 14:14:17 +0900

    properly commented everything out

--- app/javascript/components/home.js
+++ app/javascript/components/home.js
@@ -1,13 +1,13 @@
-import Typed from 'typed.js';
+// import Typed from 'typed.js';
 
-const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Towards a more positive future, together."],
-    typespeed: 40,
-    showCursor: false,
-    // loop: true
-    // startDelay: 500,
-  });
-}
+// const loadDynamicBannerText = () => {
+//   new Typed('#banner-typed-text', {
+//     strings: ["Towards a more positive future, together."],
+//     typespeed: 40,
+//     showCursor: false,
+//     // loop: true
+//     // startDelay: 500,
+//   });
+// }
 
-export { loadDynamicBannerText };
+// export { loadDynamicBannerText };
