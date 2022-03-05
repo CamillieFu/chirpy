a7514b2f18c82dad60d38db959b3edf3777b3746
Gary Yau <gary.yau@hotmail.com>
2022-03-05 12:03:48 +0900

    change back to button press

--- chirpy_extension/Chirpy_chrome_extension/scripts/get_tweets.js
+++ chirpy_extension/Chirpy_chrome_extension/scripts/get_tweets.js
@@ -1,4 +1,42 @@
-const interval = setInterval(function () {
+// const interval = setInterval(function () {
+//   document.querySelectorAll("article").forEach((tweet) => {
+//     let c_list = Array.from(tweet.classList);
+//     if (!c_list.includes("checked")) {
+//       fetch("https://www.chirpyapp.net/api/v1/statistics", {
+//         method: "POST",
+//         body: JSON.stringify({
+//           content: tweet.innerText,
+//         }),
+//         headers: {
+//           "Content-type": "application/json; charset=UTF-8",
+//           "X-User-Email": "mom@gmail.com",
+//           "X-User-Token": "o6XfWTy3Q7wHXykn7vBW",
+//         },
+//       })
+//         .then((response) => response.json())
+//         .then((data) => {
+//           if (data["bad"] === "true") {
+//             tweet.style.display = "none";
+//             tweet.classList.add("checked");
+//             console.log("bad");
+//           } else if (data["bad"] === "false") {
+//             tweet.classList.add("checked");
+//             console.log("not bad");
+//           } else {
+//             console.log(data);
+//           }
+//         });
+//     }
+//   });
+// }, 1000);
+
+// interval;
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.action === "get_tweets") get_tweets();
+});
+
+function get_tweets() {
   document.querySelectorAll("article").forEach((tweet) => {
     let c_list = Array.from(tweet.classList);
     if (!c_list.includes("checked")) {
@@ -28,42 +66,4 @@ const interval = setInterval(function () {
         });
     }
   });
-}, 10000);
-
-interval;
-
-// chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-//   if (request.action === "get_tweets") get_tweets();
-// });
-
-// function get_tweets() {
-//   document.querySelectorAll("article").forEach((tweet) => {
-//     let c_list = Array.from(tweet.classList);
-//     if (!c_list.includes("checked")) {
-//       fetch("https://www.chirpyapp.net/api/v1/statistics", {
-//         method: "POST",
-//         body: JSON.stringify({
-//           content: tweet.innerText,
-//         }),
-//         headers: {
-//           "Content-type": "application/json; charset=UTF-8",
-//           "X-User-Email": "mom@gmail.com",
-//           "X-User-Token": "o6XfWTy3Q7wHXykn7vB",
-//         },
-//       })
-//         .then((response) => response.json())
-//         .then((data) => {
-//           if (data["bad"] === "true") {
-//             tweet.style.display = "none";
-//             tweet.classList.add("checked");
-//             console.log("bad");
-//           } else if (data["bad"] === "false") {
-//             tweet.classList.add("checked");
-//             console.log("not bad");
-//           } else {
-//             console.log(data);
-//           }
-//         });
-//     }
-//   });
-// }
+}
