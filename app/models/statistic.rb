43fb2ee91f19e08bde006c5e08c57945a070e0f7
Camille Fujisaki <cae224@cornell.edu>
2022-03-07 12:46:08 +0900

    made a tentative way to add blocked words to pie chart

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -1,6 +1,7 @@
 class Statistic < ApplicationRecord
   belongs_to :kid
 
+  # not sure if it will appear with an underscore or not. If it does, maybe a different word would be better.
   enum tone: { joy: 0, analytical: 1, tentative: 2,
-                 confident: 3, fear: 4, sadness: 5, anger: 6, unverified: 7 }
+               confident: 3, fear: 4, sadness: 5, anger: 6, blocked_word: 7, unverified: 8 }
 end
