ba48beaa699621b09d799d4e6f0948436e0ce56b
Gary Yau <gary.yau@hotmail.com>
2022-03-23 20:31:33 +0900

    Made an interval to check if session storage has child_user == 'true' that clears if error occurs due to incorrect user or token

--- chirpy_extension/Chirpy_chrome_extension/scripts/get_tweets.js
+++ chirpy_extension/Chirpy_chrome_extension/scripts/get_tweets.js
@@ -1,36 +1,12 @@
-// const interval = setInterval(function () {
-//   document.querySelectorAll("article").forEach((tweet) => {
-//     let c_list = Array.from(tweet.classList);
-//     if (!c_list.includes("checked")) {
-//       fetch("https://www.chirpyapp.net/api/v1/statistics", {
-//         method: "POST",
-//         body: JSON.stringify({
-//           content: tweet.innerText,
-//         }),
-//         headers: {
-//           "Content-type": "application/json; charset=UTF-8",
-//           "X-User-Email": "mom@gmail.com",
-//           "X-User-Token": "o6XfWTy3Q7wHXykn7vBW",
-//         },
-//       })
-//         .then((response) => response.json())
-//         .then((data) => {
-//           if (data["bad"] === "true") {
-//             tweet.style.display = "none";
-//             tweet.classList.add("checked");
-//             console.log("bad");
-//           } else if (data["bad"] === "false") {
-//             tweet.classList.add("checked");
-//             console.log("not bad");
-//           } else {
-//             console.log(data);
-//           }
-//         });
-//     }
-//   });
-// }, 1000);
+const interval = setInterval(function () {
+  let = child_user = window.sessionStorage.getItem("child_user");
+  if (child_user === "true") {
+    get_tweets();
+    console.log("child working");
+  }
+}, 3000);
 
-// interval;
+interval;
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.action === "get_tweets") get_tweets();
@@ -64,6 +40,7 @@ function get_tweets() {
             console.log("not bad");
           } else {
             console.log(data);
+            clearInterval(interval);
           }
         });
     }
